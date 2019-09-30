4cefc3d1c7a187f6b2c9475c1d1b3cddf2c36746
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-30 16:53:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+monopoly ={:railroads => {} }
 end
 
 def monopoly_with_second_tier
@@ -9,6 +9,14 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  monopoly ={
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => {},
+    } 
+    
+    
+}
 end
 
 def monopoly_with_third_tier
@@ -16,11 +24,23 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+    monopoly ={
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => {:first => 1,:second => 2,:third => 3,:fourth => 4},
+    } 
+    
+    
+}
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+    monopoly ={
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => {:first => 1,:second => 2,:third => 3,:fourth => 4},
+      :names => {:north => "Pennsylvania", :south => "xyz"}
+    } 
+    
+}
 end
