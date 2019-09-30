76236e5f16590c43d364e37da2e9bd0aa005c286
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-30 17:03:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
     {
       :reading_railroad=>
       {
-        
+        "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
@@ -52,6 +52,7 @@ def monopoly_with_third_tier
         "mortgage_value"=>"$800"
       }
     } 
+    }
     
     
 }
