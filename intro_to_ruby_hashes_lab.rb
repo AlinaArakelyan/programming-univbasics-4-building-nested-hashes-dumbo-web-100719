6236757e1f1550f3b4c40be8788e43cdb6a4e28c
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-30 16:55:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,25 +39,7 @@ def monopoly_with_fourth_tier
     :railroads => {
       :pieces => 4, 
       :rent_in_dollars => {:first => 1,:second => 2,:third => 3,:fourth => 4},
-      :names => {:north => "Pennsylvania", :south => "xyz"}
-    } 
-    
-}
-end
-
-monopoly =  {
-  :railroads=>
-  {
-    :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
-    :names=>
-    {
+      :names =>  {
       :reading_railroad=>
       {
         "mortgage_value"=>"$100"
@@ -75,5 +57,40 @@ monopoly =  {
         "mortgage_value"=>"$800"
       }
     }
-  }
+    } 
+    
 }
+end
+
+# monopoly =  {
+#   :railroads=>
+#   {
+#     :pieces=>4,
+#     :rent_in_dollars=>
+#     {
+#       :one_piece_owned=>25,
+#       :two_pieces_owned=>50,
+#       :three_pieces_owned=>100,
+#       :four_pieces_owned=>200
+#     },
+#     :names=>
+#     {
+#       :reading_railroad=>
+#       {
+#         "mortgage_value"=>"$100"
+#       },
+#       :pennsylvania_railroad=>
+#       {
+#         "mortgage_value"=>"$200"
+#       },
+#       :b_and_o_railroad=>
+#       {
+#         "mortgage_value"=>"$400"
+#       },
+#       :shortline=>
+#       {
+#         "mortgage_value"=>"$800"
+#       }
+#     }
+#   }
+# }
